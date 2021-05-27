4e08a759a27700761cc3e36526210c4a794483be
Bowen Wu <51037053+Software-Cat@users.noreply.github.com>
2021-05-27 21:30:05 +1000

    Revise AppTest

--- src/test/java/io/github/softwarecats/uno/AppTest.java
+++ src/test/java/io/github/softwarecats/uno/AppTest.java
@@ -1,7 +1,7 @@
 package io.github.softwarecats.uno;
 
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
