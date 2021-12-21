0fb5a9e87361f697a994eeefcd141a2c79556aa1
Software-Cat <51037053+Software-Cat@users.noreply.github.com>
2021-12-21 19:53:40 +1100

    Update copyright

--- SECURITY.md
+++ SECURITY.md
@@ -2,13 +2,14 @@
 
 ## Supported Versions
 
-| Version | Supported          |
-| ------- | ------------------ |
-| 0.x.x   | :x:                |
+| Version | Supported |
+|---------|-----------|
+| 0.x.x   | :x:       |
 
 ## Reporting a Vulnerability
 
-To report a security vulnerability, please use the [Security Advisory](https://github.com/SoftwareCats/Casino/security/advisories/new)
+To report a security vulnerability, please use
+the [Security Advisory](https://github.com/SoftwareCats/Casino/security/advisories/new)
 tab to create a new draft advisory, where the vulnerability can be discussed and fixed.
 
 ## Security Measures
--- src/main/java/io/github/softwarecats/uno/agent/AI.java
+++ src/main/java/io/github/softwarecats/uno/agent/AI.java
@@ -1,14 +1,14 @@
 /*
  * Copyright © Bowen Wu 2021
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the “License”);
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
+ * distributed under the License is distributed on an “AS IS” BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -24,12 +24,14 @@ import org.jetbrains.annotations.NotNull;
 public class AI extends Player {
 
     @Override
-    public @NotNull Play play(Round round) {
+    public @NotNull
+    Play play(Round round) {
         return NullCard.getInstance().getPlay(this);
     }
 
     @Override
-    public @NotNull Play playAfterDrawing(Round round, Card cardDrawn) {
+    public @NotNull
+    Play playAfterDrawing(Round round, Card cardDrawn) {
         return NullCard.getInstance().getPlay(this);
     }
 
