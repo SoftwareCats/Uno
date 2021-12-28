13a6f05c811d4b9b31b7f1a680d6e74e3075b13d
Software-Cat <51037053+Software-Cat@users.noreply.github.com>
2021-12-28 18:26:11 +1100

    Fix DeckBuilder generating too many action cards bug

--- src/main/java/io/github/softwarecats/uno/util/DeckBuilder.java
+++ src/main/java/io/github/softwarecats/uno/util/DeckBuilder.java
@@ -3,8 +3,10 @@ package io.github.softwarecats.uno.util;
 import io.github.softwarecats.uno.card.*;
 import io.github.softwarecats.uno.card.base.Card;
 import io.github.softwarecats.uno.card.base.Color;
+import lombok.NonNull;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class DeckBuilder {
@@ -12,10 +14,64 @@ public class DeckBuilder {
     private DeckBuilder() {
     }
 
-    public static List<Card> getDeck() {
+    public static @NonNull List<Card> getDeck() {
         List<Card> deck = new ArrayList<>();
 
         // Number cards.
+        generateNumberCards(deck);
+
+        // Action cards.
+        generateSkipCards(deck);
+        generateReverseCards(deck);
+        generateDrawTwoCards(deck);
+
+        // Wild cards.
+        generateWildCards(deck);
+        generateWildDrawFourCards(deck);
+
+        return deck;
+    }
+
+    protected static void generateWildCards(Collection<Card> deck) {
+        for (int i = 0; i < 4; i++) {
+            deck.add(new WildCard());
+        }
+    }
+
+    protected static void generateWildDrawFourCards(Collection<Card> deck) {
+        for (int i = 0; i < 4; i++) {
+            deck.add(new WildDrawFourCard());
+        }
+    }
+
+    protected static void generateSkipCards(Collection<Card> deck) {
+        // Two skip cards for each color.
+        for (Color color : Color.values()) {
+            for (int i = 0; i < 2; i++) {
+                deck.add(new SkipCard(color));
+            }
+        }
+    }
+
+    protected static void generateReverseCards(Collection<Card> deck) {
+        // Two reverse cards for each color.
+        for (Color color : Color.values()) {
+            for (int i = 0; i < 2; i++) {
+                deck.add(new ReverseCard(color));
+            }
+        }
+    }
+
+    protected static void generateDrawTwoCards(Collection<Card> deck) {
+        // Two draw two cards for each color.
+        for (Color color : Color.values()) {
+            for (int i = 0; i < 2; i++) {
+                deck.add(new DrawTwoCard(color));
+            }
+        }
+    }
+
+    protected static void generateNumberCards(Collection<Card> deck) {
         for (Color color : Color.values()) {
             // Only 1 zero card for each color.
             deck.add(new NumberCard(color, 0));
@@ -27,22 +83,5 @@ public class DeckBuilder {
                 }
             }
         }
-
-        // Action cards.
-        for (Color color : Color.values()) {
-            for (int i = 0; i < 8; i++) {
-                deck.add(new SkipCard(color));
-                deck.add(new ReverseCard(color));
-                deck.add(new DrawTwoCard(color));
-            }
-        }
-
-        // Wildcards.
-        for (int i = 0; i < 4; i++) {
-            deck.add(new WildCard());
-            deck.add(new WildDrawFourCard());
-        }
-
-        return deck;
     }
 }
